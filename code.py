cab84b2f7128ef9f6e63f76bb59a7dc31677d5c6
Saif <seif.abdelkefi@gmail.com>
2024-11-11 14:10:23 -0500

    api changes + few comments to frame future work

--- code.py
+++ code.py
@@ -4,7 +4,15 @@ import json
 saif_key="bf293b62915341ab96753bafd9e2da75"
 
 sources= f"https://newsapi.org/v2/top-headlines/sources?country=us&apiKey={saif_key}"
-natenyahu= f"https://newsapi.org/v2/everything?q=netanyahu&apiKey={saif_key}"
+natenyahu= f'https://newsapi.org/v2/everything?q=+netanyahu&searchIn=title&apiKey={saif_key}'
+# cannot specify a country filter with the endpoint /everything, we have to manually filter the sources
+# Api defaults
+    #gives one page worth of result and each page has 100 returns by default
+    # sorts by publishedAt, newest articles come first
+# we need to think about tweeking the paramters to suit our needs, a lot of decisions will be taken and need to be documented
+    # diversify timings
+    # diversify sources based on political spectrum
+
 
 response = requests.get(natenyahu)
 
