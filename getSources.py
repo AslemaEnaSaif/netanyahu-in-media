9eb6f07665264e135008958e343f2c5a9c779f96
n4z03 <149629013+n4z03@users.noreply.github.com>
2024-11-11 16:43:50 -0500

    tool to get the available sources for each country

---
+++ getSources.py
@@ -0,0 +1,23 @@
+import requests
+
+nazifa_key = '8d1ef3a971f1422aac484c57ddba03b0'
+
+url = 'https://newsapi.org/v2/sources'
+country = "ca"
+
+params = {
+    'language': 'en',
+    'country': country,
+    'apiKey': nazifa_key
+}
+
+response = requests.get(url, params=params)
+
+if (response.status_code == 200):
+    data = response.json()
+    sources = data.get('sources', [])
+    
+    for source in sources:
+        print(f"{source['name']}")
+else:
+    print('failed')        
